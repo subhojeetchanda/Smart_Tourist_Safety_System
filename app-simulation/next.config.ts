d5ccc08db8c2fe92c998e7fdf53fd6aa84c3a29d
Subhojeet Chanda <subhojeetchanda18@gmail.com>
2026-09-06 12:33:22 +0530

    Ignore ESLint and TS errors during Vercel builds

--- app-simulation/next.config.ts
+++ app-simulation/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/simulator',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
